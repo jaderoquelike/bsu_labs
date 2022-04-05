add62b107c249162ebfded90ba65cf06dc714adc
roga4 <69800021+jaderoquelike@users.noreply.github.com>
2022-04-05 21:41:36 +0300

    Add files via upload

---
+++ laba9(1).cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program {
+  public static void Main () {
+    const int N = 5;
+    int[,] arr = new int[N,N];
+    int[] arrX = new int[5]{1, 1, 1, 1, 1};
+    for (int i = 0; i < N; i++){
+      for (int j = 0; j < N; j++){
+        arr[i,j] = int.Parse(Console.ReadLine());
+      }
+    }
+    for (int i = 0; i < N; i++){
+      for (int j = 0; j < N; j++){
+        if (i > j) arrX[i] *= arr[i,j];
+      }
+    }
+    for (int i = 0; i<N; i++){
+      Console.WriteLine( "Значение равно: " + arrX[i] + ' ');
+    }
+}
+}
